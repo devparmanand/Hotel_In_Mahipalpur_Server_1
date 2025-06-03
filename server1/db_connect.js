0b360a844ebfbfbfae4dbd0088ed9fc952c533f9
dev-ankit24 <ankitkumar839282@gmail.com>
2025-06-03 13:42:40 +0530

    bug fix

--- server1/db_connect.js
+++ server1/db_connect.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 async function getConnect() {
     mongoose.connect('mongodb://127.0.0.1:27017/him_backend_1')
     .then(()=>{
-        console.log("Database is Connected");
+        console.log("Database is Connected from him_backend_1");
         
     })
     .catch((error)=>{
